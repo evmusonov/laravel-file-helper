db97acff602f9b76fb5d503bbe846188dfd67346
evmusonov <evmusonov@yandex.ru>
2020-02-02 09:28:56 +0700

    feat: add migration

--- src/FileHelperServiceProvider.php
+++ src/FileHelperServiceProvider.php
@@ -11,6 +11,9 @@ class FileHelperServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/config/filehelper.php' => config_path('filehelper.php'),
         ]);
+
+        // Load migrations
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
     }
 
     public function register()
